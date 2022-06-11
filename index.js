d25998ed2d9ffec6ebb36d4014cfad7fc839ae0d
Ryan Ridwan <ryanridnn@gmail.com>
2022-06-11 11:37:20 +0700

    another

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ const app = express();
 
 app.use(
 	cors({
-		allowedHeaders: "*",
-		allowMethods: "*",
-		allowOrigin: "*",
 		origin: CLIENT_URL,
 		credentials: true,
 	})
@@ -33,6 +30,15 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+	// res.header("Access-Control-Allow-Origin", "*");
+	// res.header("Access-Control-Allow-Headers", "*");
+	// res.header("Access-Control-Allow-Credentials", true);
+	// res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+
+	next();
+});
 
 app.use(
 	session({
